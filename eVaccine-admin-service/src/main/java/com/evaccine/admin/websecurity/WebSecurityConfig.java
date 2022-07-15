67cebe7cfe5b5bc73c78b53e65a85e0f09bbb16c
unknown <nakattamudi@gmail.com>
2022-07-15 20:48:01 +0530

    minor changes for websecurity adapter

--- eVaccine-admin-service/src/main/java/com/evaccine/admin/websecurity/WebSecurityConfig.java
+++ eVaccine-admin-service/src/main/java/com/evaccine/admin/websecurity/WebSecurityConfig.java
@@ -4,14 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-	@Override
+public class WebSecurityConfig {
+
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().anyRequest().permitAll().and().csrf().disable().sessionManagement()
 				.sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().httpBasic();
