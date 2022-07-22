a83ac2c271a05856295b51ddc8012a849ac73d0a
nakattamudi <venu90100@gmail.com>
2022-07-22 19:44:55 +0530

    added regex patterns for new user registration

--- evaccine-user-service/src/main/java/com/evaccine/user/constants/UserServiceConstants.java
+++ evaccine-user-service/src/main/java/com/evaccine/user/constants/UserServiceConstants.java
@@ -2,25 +2,24 @@ package com.evaccine.user.constants;
 
 public class UserServiceConstants {
 
-
     public static final String USER = "user";
     public static final String REGISTER_USER_INFO_MAPPING = "register";
     public static final String FETCH_USER_INFO_MAPPING = "fetch/{aadhar}";
     public static final String UPDATE_USER_INFO_MAPPING = "update";
-
     public static final int MAX_ALLOWED_USERNAME_LENGTH = 50;
     public static final int MAX_ALLOWED_COUNTRY_CODE_LENGTH = 3;
     public static final String USER_NAME_MANDATORY = "userName is Mandatory";
-    public static final String COUNTRY_NAME_MAX_LENGTH_MESSAGE = "userName should not be more than "
+    public static final String USER_NAME_MAX_LENGTH_MESSAGE = "userName should not be more than "
             + MAX_ALLOWED_USERNAME_LENGTH + " char";
-
     public static final String AADHAR_MANDATORY = "aadhar is Mandatory";
+    public static final String AADHAR_PATTERN = "^[2-9]{1}[0-9]{3}[\\s]?[0-9]{4}[\\s]?[0-9]{4}$";
     public static final String AADHAR_INVALID_MESSAGE = "Invalid aadhar provided.Please check";
+    public static final String EMAIL_ID_PATTERN = "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$";
     public static final String EMAIL_ID_MANDATORY = "emailId is Mandatory";
     public static final String EMAIL_ID_INVALID_MESSAGE = "Invalid emailId provided.Please check";
-    public static final String MOBILE_NAME_MANDATORY = "mobileNumber is Mandatory";
-    public static final String MOBILE_INVALID_MESSAGE = "Invalid mobile provided.Please check";
-
+    public static final String MOBILE_NUMBER_MANDATORY = "mobileNumber is Mandatory";
+    public static final String MOBILE_NUMBER_PATTERN = "^[1-9]{1}[0-9]{9}$";
+    public static final String MOBILE_NUMBER_INVALID_MESSAGE = "Invalid mobileNumber provided.Please check";
     public static final String COUNTRY_CODE_MANDATORY = "countryCode is Mandatory";
     public static final String COUNTRY_CODE_MAX_LENGTH_MESSAGE = "countryCode should not be more than "
             + MAX_ALLOWED_COUNTRY_CODE_LENGTH + " char";
--- evaccine-user-service/src/main/java/com/evaccine/user/validator/UserServiceValidator.java
+++ evaccine-user-service/src/main/java/com/evaccine/user/validator/UserServiceValidator.java
@@ -3,16 +3,21 @@ package com.evaccine.user.validator;
 
 import static com.evaccine.user.constants.UserServiceConstants.AADHAR_INVALID_MESSAGE;
 import static com.evaccine.user.constants.UserServiceConstants.AADHAR_MANDATORY;
+import static com.evaccine.user.constants.UserServiceConstants.AADHAR_PATTERN;
 import static com.evaccine.user.constants.UserServiceConstants.COUNTRY_CODE_MANDATORY;
 import static com.evaccine.user.constants.UserServiceConstants.COUNTRY_CODE_MAX_LENGTH_MESSAGE;
-import static com.evaccine.user.constants.UserServiceConstants.COUNTRY_NAME_MAX_LENGTH_MESSAGE;
 import static com.evaccine.user.constants.UserServiceConstants.EMAIL_ID_INVALID_MESSAGE;
 import static com.evaccine.user.constants.UserServiceConstants.EMAIL_ID_MANDATORY;
+import static com.evaccine.user.constants.UserServiceConstants.EMAIL_ID_PATTERN;
 import static com.evaccine.user.constants.UserServiceConstants.MAX_ALLOWED_COUNTRY_CODE_LENGTH;
 import static com.evaccine.user.constants.UserServiceConstants.MAX_ALLOWED_USERNAME_LENGTH;
-import static com.evaccine.user.constants.UserServiceConstants.MOBILE_INVALID_MESSAGE;
-import static com.evaccine.user.constants.UserServiceConstants.MOBILE_NAME_MANDATORY;
+import static com.evaccine.user.constants.UserServiceConstants.MOBILE_NUMBER_INVALID_MESSAGE;
+import static com.evaccine.user.constants.UserServiceConstants.MOBILE_NUMBER_MANDATORY;
+import static com.evaccine.user.constants.UserServiceConstants.MOBILE_NUMBER_PATTERN;
 import static com.evaccine.user.constants.UserServiceConstants.USER_NAME_MANDATORY;
+import static com.evaccine.user.constants.UserServiceConstants.USER_NAME_MAX_LENGTH_MESSAGE;
+
+import java.util.regex.Pattern;
 
 import com.evaccine.user.model.UserRegisterRequest;
 import io.micrometer.core.instrument.util.StringUtils;
@@ -24,23 +29,21 @@ public class UserServiceValidator {
 
     public boolean validateUserDetails(final UserRegisterRequest userRegisterRequest) {
 
-
         Assert.isTrue(StringUtils.isNotBlank(userRegisterRequest.getUserName()), USER_NAME_MANDATORY);
         Assert.isTrue(userRegisterRequest.getUserName().length() < MAX_ALLOWED_USERNAME_LENGTH,
-                COUNTRY_NAME_MAX_LENGTH_MESSAGE);
+                USER_NAME_MAX_LENGTH_MESSAGE);
 
         Assert.isTrue(StringUtils.isNotBlank(userRegisterRequest.getAadharNumber()), AADHAR_MANDATORY);
-        Assert.isTrue(userRegisterRequest.getAadharNumber().length() < 23,
+        Assert.isTrue(Pattern.matches(AADHAR_PATTERN, userRegisterRequest.getAadharNumber()),
                 AADHAR_INVALID_MESSAGE);
 
         Assert.isTrue(StringUtils.isNotBlank(userRegisterRequest.getEmailId()), EMAIL_ID_MANDATORY);
-        Assert.isTrue(userRegisterRequest.getEmailId().length() < 23,
+        Assert.isTrue(Pattern.matches(EMAIL_ID_PATTERN, userRegisterRequest.getEmailId()),
                 EMAIL_ID_INVALID_MESSAGE);
 
-        Assert.isTrue(StringUtils.isNotBlank(userRegisterRequest.getMobileNumber()), MOBILE_NAME_MANDATORY);
-        Assert.isTrue(userRegisterRequest.getMobileNumber().length() < 23,
-                MOBILE_INVALID_MESSAGE);
-
+        Assert.isTrue(StringUtils.isNotBlank(userRegisterRequest.getMobileNumber()), MOBILE_NUMBER_MANDATORY);
+        Assert.isTrue(Pattern.matches(MOBILE_NUMBER_PATTERN, userRegisterRequest.getMobileNumber()),
+                MOBILE_NUMBER_INVALID_MESSAGE);
 
         Assert.isTrue(StringUtils.isNotBlank(userRegisterRequest.getCountryCode()), COUNTRY_CODE_MANDATORY);
         Assert.isTrue(userRegisterRequest.getCountryCode().length() < MAX_ALLOWED_COUNTRY_CODE_LENGTH,
