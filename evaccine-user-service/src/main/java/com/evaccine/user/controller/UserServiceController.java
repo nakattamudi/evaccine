28bea8d63d876bcb7318362b0ff6bf6e024081fe
nakattamudi <venu90100@gmail.com>
2022-07-24 21:52:59 +0530

    Swagger Doc changes

--- evaccine-user-service/src/main/java/com/evaccine/user/controller/UserServiceController.java
+++ evaccine-user-service/src/main/java/com/evaccine/user/controller/UserServiceController.java
@@ -14,6 +14,7 @@ import com.evaccine.user.model.UserRegisterResponse;
 import com.evaccine.user.model.UserVaccineInfoResponse;
 import com.evaccine.user.model.VaccineRegisterRequest;
 import com.evaccine.user.service.UserService;
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
@@ -32,12 +33,20 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 @RequestMapping(USER)
+@Api(value = "userservice", description = "CRUD Operations pertaining to user registration for vaccine")
 public class UserServiceController {
 
     @Autowired
     private UserService userService;
 
     @PostMapping(REGISTER_USER_INFO_MAPPING)
+    @ApiOperation(value = "Register User Details within the System", response = UserRegisterResponse.class)
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Success"),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error"),
+            @ApiResponse(responseCode = "400", description = "Bad Request"),
+            @ApiResponse(responseCode = "401", description = "You are not authorized to view the resource"),
+            @ApiResponse(responseCode = "403", description = "Access forbidden"),
+            @ApiResponse(responseCode = "404", description = "The resource you were trying to reach is not found")})
     public ResponseEntity<UserRegisterResponse> registerUserDetails(@RequestBody final UserRegisterRequest userDetailsRequest) {
         log.info("registerUserDetails: userDetailsRequest : {} ", userDetailsRequest);
         UserRegisterResponse userRegisterResponse = userService.registerUserDetails(userDetailsRequest);
@@ -45,6 +54,13 @@ public class UserServiceController {
     }
 
     @GetMapping(FETCH_USER_INFO_MAPPING)
+    @ApiOperation(value = "Fetch User Information based on Id provided", response = UserRegisterResponse.class)
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Success"),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error"),
+            @ApiResponse(responseCode = "400", description = "Bad Request"),
+            @ApiResponse(responseCode = "401", description = "You are not authorized to view the resource"),
+            @ApiResponse(responseCode = "403", description = "Access forbidden"),
+            @ApiResponse(responseCode = "404", description = "The resource you were trying to reach is not found")})
     public ResponseEntity<UserRegisterResponse> getUserDetails(@PathVariable(name = "aadhar") final String id) {
         log.info("getUserDetails: id : {} ", id);
         UserRegisterResponse userRegisterResponse = userService.getUserDetails(id);
@@ -52,6 +68,13 @@ public class UserServiceController {
     }
 
     @PutMapping(UPDATE_USER_INFO_MAPPING)
+    @ApiOperation(value = "Update User Information based on Id provided", response = UserRegisterResponse.class)
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Success"),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error"),
+            @ApiResponse(responseCode = "400", description = "Bad Request"),
+            @ApiResponse(responseCode = "401", description = "You are not authorized to view the resource"),
+            @ApiResponse(responseCode = "403", description = "Access forbidden"),
+            @ApiResponse(responseCode = "404", description = "The resource you were trying to reach is not found")})
     public ResponseEntity<UserRegisterResponse> updateUserDetails(@RequestBody final UserRegisterRequest userDetailsRequest) {
         log.info("updateUserDetails : userDetailsRequest : {} ", userDetailsRequest);
         UserRegisterResponse userRegisterResponse = userService.updateUserDetails(userDetailsRequest);
@@ -72,7 +95,15 @@ public class UserServiceController {
         List<UserVaccineInfoResponse> countryInfoResponseList = userService.fetchVaccineInfo(countryCode, pincode);
         return new ResponseEntity<>(countryInfoResponseList, HttpStatus.OK);
     }
+
     @PostMapping(REGISTER_USER_FOR_VACCINATION)
+    @ApiOperation(value = "Register user for Vaccination", response = UserRegisterResponse.class)
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Success"),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error"),
+            @ApiResponse(responseCode = "400", description = "Bad Request"),
+            @ApiResponse(responseCode = "401", description = "You are not authorized to view the resource"),
+            @ApiResponse(responseCode = "403", description = "Access forbidden"),
+            @ApiResponse(responseCode = "404", description = "The resource you were trying to reach is not found")})
     public ResponseEntity<UserRegisterResponse> registerUserForVaccination(
             @RequestBody final VaccineRegisterRequest vaccineRegisterRequest) {
         log.info("registerUserForVaccination : userDetailsRequest : {} ", vaccineRegisterRequest);
